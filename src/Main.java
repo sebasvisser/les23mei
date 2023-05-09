86c68b407b69e1c15968a0e58a9c68c2c1ec16c2
sebasvisser <sebas.visser@mensenrelatie.nl>
2023-05-09 15:22:06 +0200

    oeps, import stond er nog niet in en nextString moet next zijn :)

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+
+
 // Huiswerk Opdracht Dollar Euro Converter met input via Scanner
 // Wisselkoers:
 
@@ -10,12 +12,16 @@ Pseudo Stappen plan:
 
 
  */
+// IMPORTS
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         // Dynamische Variabele voor functie aanmaken
         double dollarAmount;
         double euroAmount;
         double userAmount;
+        String userCurrency;
         // Constante Variabele aanmaken voor functie
         double euroToDollar = 1.09;
 
@@ -28,5 +34,13 @@ public class Main {
         // Bevestiging van input
         System.out.println("Je hebt ingegeven dat je: " + userAmount + "wilt converteren.");
 
+        // Scanner aanmaken voor bedrag
+        Scanner scannerCurrency = new Scanner(System.in);
+        // Input van gebruiker vragen en toekennen aan variabele
+        System.out.println("Welke valuta heb jij die je wilt omrekenen? Dollar of Euro?");
+        userCurrency = scannerCurrency.next();
+        scannerCurrency.close();
+        // Bevestiging van input
+        System.out.println("Je hebt ingegeven dat je: " + userCurrency + "hebt en wilt omzetten.");
     }
 }
